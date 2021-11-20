d2aa8d3bc55913f14678d026fd0a2d4af41e1f19
Pranav Baburaj <diamonddevstuff@gmail.com>
2021-11-20 15:29:06 +0530

    feat:Basic files

--- profile/README.md
+++
@@ -1,3 +0,0 @@
-# Technetium
-
-<img src="https://avatars.githubusercontent.com/u/79608367?s=200&v=4">
---
+++ src/exception.ts
@@ -0,0 +1,40 @@
+import { redBright, yellowBright } from "chalk";
+
+interface ErrorMessage {
+  // The error message
+  message: string;
+  // Any suggestion that can help in solving
+  // the issue
+  suggestion?: string;
+}
+
+export class Exception {
+  private readonly message: string;
+  private readonly suggestion?: string;
+
+  /**
+   * @constructor
+   * @param {ErrorMessage} message The error message and the suggestion message
+   * @param {boolean} isFatal Whether the program should exit after throwing the errpr
+   * @param {number} exitStatusCode The exit status code
+   */
+  constructor(
+    message: ErrorMessage,
+    isFatal: boolean = true,
+    exitStatusCode: number = 1
+  ) {
+    this.message = message.message;
+    this.suggestion = message.suggestion;
+    this.throw(isFatal, exitStatusCode);
+  }
+
+  private throw(isFatal: boolean, exitStatusCode: number) {
+    console.log(redBright(this.message));
+    if (this.suggestion) {
+      console.log(yellowBright(this.suggestion));
+    }
+    if (isFatal) {
+      process.exit(exitStatusCode);
+    }
+  }
+}
